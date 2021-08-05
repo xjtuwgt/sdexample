e14b59d5d76ca750e2d039cea9b68c7049240516
guangtao.wang <guangtao.wang@jd.com>
2021-08-05 09:20:56 -0700

    example

--- toyexample_datageneration.py
+++ toyexample_datageneration.py
@@ -10,29 +10,41 @@ if __name__ == '__main__':
                         type=str,
                         default=join(HOME_DATA_FOLDER, 'toy_data'),
                         help='Directory to save row_data')
-    parser.add_argument('--train_data_size', type=int, default=1000, help='train data size')
+    parser.add_argument('--train_data_size', type=int, default=100, help='train data size')
+    parser.add_argument('--train_pos_label_ratio', type=float, default=0.5, help='label distribution')
     parser.add_argument('--target_tokens', type=str, default='cat')
-    parser.add_argument('--seq_len', type=int, default=300)
-    parser.add_argument('--test_data_size', type=int, default=10000, help='test data size')
+    parser.add_argument('--train_seq_len', type=str, default='300')
     parser.add_argument('--train_seed', type=int, default=42, help='random seed for training data generation')
+
+    parser.add_argument('--test_data_size', type=int, default=10000, help='test data size')
+    parser.add_argument('--test_seq_len', type=str, default='300')
+    parser.add_argument('--test_pos_label_ratio', type=float, default=0.5, help='test label distribution')
     parser.add_argument("--test_seed", type=int, default=1234, help='random seed for testing data generation')
+
     args = parser.parse_args()
+    for key, value in vars(args):
+        print('Parameter: {}\t{}'.format(key, value))
 
     os.makedirs(args.data_dir, exist_ok=True)
 
-    train_data_set = FindCatDataset(total_examples=args.train_data_size,
-                                    target_tokens=args.target_tokens,
-                                    seqlen=args.seq_len,
-                                    seed=args.train_seed)
-    train_data_file_name = join(args.data_dir, 'train_' + args.target_tokens + '_' + str(args.train_data_size)
-                                + '_' + str(args.train_seed) + '_' + args.seq_len + '.pkl.gz')
-
-    train_data_set.save_data_into_file(data_file_name=train_data_file_name)
-
+    # train_seq_len = args.train_seq_len
+    # train_seq_len = tuple([int(x) for x in train_seq_len.split(',')])
+    # train_data_set = FindCatDataset(total_examples=args.train_data_size,
+    #                                 target_tokens=args.target_tokens,
+    #                                 seqlen=train_seq_len,
+    #                                 seed=args.train_seed)
+    # train_data_file_name = join(args.data_dir, 'train_' + args.target_tokens + '_' + str(args.train_data_size)
+    #                             + '_' + str(args.train_seed) + '_' + str(args.train_seq_len) + '_' +
+    #                             str(args.train_pos_label_ratio) + '.pkl.gz')
+    # train_data_set.save_data_into_file(data_file_name=train_data_file_name)
+    # ##++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    test_seq_len = args.test_seq_len
+    test_seq_len = tuple([int(x) for x in test_seq_len.split(',')])
     test_data_set = FindCatDataset(total_examples=args.test_data_size,
                                    target_tokens=args.target_tokens,
-                                   seqlen=args.seq_len,
+                                   seqlen=test_seq_len,
                                    seed=args.test_seed)
     test_data_file_name = join(args.data_dir, 'test_' + args.target_tokens + '_' + str(args.test_data_size)
-                                + '_' + str(args.test_seed) + '_' + args.seq_len + '.pkl.gz')
+                                + '_' + str(args.test_seed) + '_' + args.test_seq_len + '_' +
+                               str(args.test_pos_label_ratio) + '.pkl.gz')
     test_data_set.save_data_into_file(data_file_name=test_data_file_name)
