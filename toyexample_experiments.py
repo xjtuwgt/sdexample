941f25a61134f5edc1624865eaf2d180a32ede06
guangtao.wang <guangtao.wang@jd.com>
2021-08-07 22:13:03 -0700

    example

--- toyexample_experiments.py
+++ toyexample_experiments.py
@@ -99,7 +99,7 @@ if __name__ == "__main__":
 
     parser.add_argument('--batch_size', type=int, default=32)
     parser.add_argument('--test_batch_size', type=int, default=64)
-    parser.add_argument('--window_size', type=int, default=20)
+    parser.add_argument('--window_size', type=int, default=100)
     parser.add_argument('--eval_batch_interval_num', type=int, default=100)
 
     parser.add_argument('--model_name', type=str, default='bert-base-uncased')
