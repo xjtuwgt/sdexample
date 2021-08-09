7d3ea3f0a930ab8c0b61a32e7a6bb59d15121ea0
guangtao.wang <guangtao.wang@jd.com>
2021-08-09 15:09:56 -0700

    example

--- toyexample_datageneration.py
+++ toyexample_datageneration.py
@@ -15,6 +15,7 @@ if __name__ == '__main__':
     parser.add_argument('--train_pos_label_ratio', type=float, default=0.5, help='label distribution')
     parser.add_argument('--target_tokens', type=str, default='cat')
     parser.add_argument('--train_seq_len', type=str, default='300')
+    parser.add_argument('--train_target_position', type=str, default='3,10,20')
     parser.add_argument('--train_seed', type=int, default=42, help='random seed for training data generation')
 
     parser.add_argument('--test_data_size', type=int, default=10000, help='test data size')
@@ -28,27 +29,27 @@ if __name__ == '__main__':
         print('Parameter: {}\t{}'.format(key, value))
 
     os.makedirs(args.data_dir, exist_ok=True)
-    # train_seq_len = args.train_seq_len
-    # train_seq_len = tuple([int(x) for x in train_seq_len.split(',')])
-    # train_data_set = FindCatDataset(total_examples=args.train_data_size,
-    #                                 target_tokens=args.target_tokens,
-    #                                 seqlen=train_seq_len,
-    #                                 prob=args.train_pos_label_ratio,
-    #                                 multi_target=args.multi_target in ['multi'],
-    #                                 seed=args.train_seed)
-    # train_data_file_name = join(args.data_dir, 'train_' + args.multi_target + '_' + args.target_tokens + '_' + str(args.train_data_size)
-    #                             + '_' + str(args.train_seed) + '_' + str(args.train_seq_len) + '_' +
-    #                             str(args.train_pos_label_ratio) + '.pkl.gz')
-    # train_data_set.save_data_into_file(data_file_name=train_data_file_name)
+    train_seq_len = args.train_seq_len
+    train_seq_len = tuple([int(x) for x in train_seq_len.split(',')])
+    train_data_set = FindCatDataset(total_examples=args.train_data_size,
+                                    target_tokens=args.target_tokens,
+                                    seqlen=train_seq_len,
+                                    prob=args.train_pos_label_ratio,
+                                    multi_target=args.multi_target in ['multi'],
+                                    seed=args.train_seed)
+    train_data_file_name = join(args.data_dir, 'train_' + args.multi_target + '_' + args.target_tokens + '_' + str(args.train_data_size)
+                                + '_' + str(args.train_seed) + '_' + str(args.train_seq_len) + '_' +
+                                str(args.train_pos_label_ratio) + '.pkl.gz')
+    train_data_set.save_data_into_file(data_file_name=train_data_file_name)
     ##++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-    test_seq_len = args.test_seq_len
-    test_seq_len = tuple([int(x) for x in test_seq_len.split(',')])
-    test_data_set = FindCatDataset(total_examples=args.test_data_size,
-                                   target_tokens=args.target_tokens,
-                                   seqlen=test_seq_len,
-                                   multi_target=args.multi_target in ['multi'],
-                                   seed=args.test_seed)
-    test_data_file_name = join(args.data_dir, 'test_' + args.multi_target + '_' + args.target_tokens + '_' + str(args.test_data_size)
-                                + '_' + str(args.test_seed) + '_' + args.test_seq_len + '_' +
-                               str(args.test_pos_label_ratio) + '.pkl.gz')
-    test_data_set.save_data_into_file(data_file_name=test_data_file_name)
+    # test_seq_len = args.test_seq_len
+    # test_seq_len = tuple([int(x) for x in test_seq_len.split(',')])
+    # test_data_set = FindCatDataset(total_examples=args.test_data_size,
+    #                                target_tokens=args.target_tokens,
+    #                                seqlen=test_seq_len,
+    #                                multi_target=args.multi_target in ['multi'],
+    #                                seed=args.test_seed)
+    # test_data_file_name = join(args.data_dir, 'test_' + args.multi_target + '_' + args.target_tokens + '_' + str(args.test_data_size)
+    #                             + '_' + str(args.test_seed) + '_' + args.test_seq_len + '_' +
+    #                            str(args.test_pos_label_ratio) + '.pkl.gz')
+    # test_data_set.save_data_into_file(data_file_name=test_data_file_name)
