29b45da17531ae8707e6c2ecf0e324e253fd3e85
guangtao.wang <guangtao.wang@jd.com>
2021-08-08 21:13:31 -0700

    example

--- toyexample_datageneration.py
+++ toyexample_datageneration.py
@@ -11,8 +11,8 @@ if __name__ == '__main__':
                         default=join(HOME_DATA_FOLDER, 'toy_data'),
                         help='Directory to save row_data')
     parser.add_argument('--multi_target', type=str, default='multi')
-    parser.add_argument('--train_data_size', type=int, default=500, help='train data size')
-    parser.add_argument('--train_pos_label_ratio', type=float, default=0.5, help='label distribution')
+    parser.add_argument('--train_data_size', type=int, default=1000, help='train data size')
+    parser.add_argument('--train_pos_label_ratio', type=float, default=0.05, help='label distribution')
     parser.add_argument('--target_tokens', type=str, default='cat')
     parser.add_argument('--train_seq_len', type=str, default='300')
     parser.add_argument('--train_seed', type=int, default=42, help='random seed for training data generation')
