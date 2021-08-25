d1fd8cbdde3743b63b2607931a625d4973deade8
guangtao.wang <guangtao.wang@jd.com>
2021-08-25 16:28:52 -0700

    first commit

--- run_experiments.sh
+++ run_experiments.sh
@@ -14,6 +14,6 @@ for ENTRY in "${JOBS_PATH}"/*.sh; do
   chmod +x $ENTRY
   FILE_NAME="$(basename "$ENTRY")"
   echo $FILE_NAME
-  /mnt/cephfs2/asr/users/ming.tu/software/kaldi/egs/wsj/s5/utils/queue.pl -q g2.q -l gpu=1 $LOGS_PATH/$FILE_NAME.log $ENTRY &
+  /mnt/cephfs2/asr/users/ming.tu/software/kaldi/egs/wsj/s5/utils/queue.pl -q g.q -l gpu=4 $LOGS_PATH/$FILE_NAME.log $ENTRY &
   sleep 20
 done
--- task_generation_script.py
+++ task_generation_script.py
@@ -22,20 +22,19 @@ def generate_animal_zeroshot_bash():
         remove_all_files(jobs_path)
     ##################################################
     with open(jobs_path + 'toy_example_zeroshot_animal.sh', 'w') as rsh_i:
-        command_i = "python -m toyexample_experiments.py --sent_dropout 0.1"
+        command_i = "CUDA_VISIBLE_DEVICES=0,1,2,3 python -m toyexample_experiments.py --sent_dropout 0.1"
         rsh_i.write(command_i)
         print('saving jobs at {}'.format(jobs_path + 'toy_example_animal_sentdrop0.1.sh'))
 
     with open(jobs_path + 'toy_example_zeroshot_animal_sentdrop0.1.sh', 'w') as rsh_i:
-        command_i = "python -m toyexample_experiments.py --sent_dropout 0.1"
+        command_i = "CUDA_VISIBLE_DEVICES=0,1,2,3 python -m toyexample_experiments.py --sent_dropout 0.1"
         rsh_i.write(command_i)
         print('saving jobs at {}'.format(jobs_path + 'toy_example_animal_sentdrop0.1.sh'))
 
     with open(jobs_path + 'toy_example__zeroshot_animal_sentdrop_beta0.1.sh', 'w') as rsh_i:
-        command_i = "python -m toyexample_experiments.py --sent_dropout 0.1 --beta_drop 'true'"
+        command_i = "CUDA_VISIBLE_DEVICES=0,1,2,3 python -m toyexample_experiments.py --sent_dropout 0.1 --beta_drop 'true'"
         rsh_i.write(command_i)
         print('saving jobs at {}'.format(jobs_path + 'toy_example_animal_sentdrop0.1.sh'))
-
 
 if __name__ == '__main__':
     generate_animal_zeroshot_bash()
