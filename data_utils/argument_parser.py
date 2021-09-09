92df693f6289ef05fe5e66069200e706933398d6
guangtao.wang <guangtao.wang@jd.com>
2021-09-09 14:42:54 -0700

    argument parser

--- data_utils/argument_parser.py
+++ data_utils/argument_parser.py
@@ -44,7 +44,7 @@ def prober_default_parser():
     parser.add_argument('--threshold', type=float, default=0.5)
 
     parser.add_argument('--batch_size', type=int, default=64)
-    parser.add_argument('--test_batch_size', type=int, default=128)
+    parser.add_argument('--test_batch_size', type=int, default=256)
     parser.add_argument('--eval_batch_interval_num', type=int, default=100)
     parser.add_argument('--lr', type=float, default=1e-5)
     parser.add_argument('--seed', type=int, default=1234)
