316eba7b7b892b4b9c82f574e7328e659f9b314b
guangtao.wang <guangtao.wang@jd.com>
2021-09-15 21:15:30 -0700

    argument parser

--- data_utils/argument_parser.py
+++ data_utils/argument_parser.py
@@ -30,7 +30,7 @@ def prober_default_parser():
     parser.add_argument('--target_tokens', type=str, default='cat')
 
     parser.add_argument('--model_name', type=str, default='bert-base-uncased')
-    parser.add_argument('--train_file_name', type=str, default='train_fastsingle_cat_10000_42_300_1.0.pkl.gz')
+    parser.add_argument('--train_file_name', type=str, default='train_fastsingle_cat_5000_42_300_1.0.pkl.gz')
     parser.add_argument('--vocab_size', type=int, default=100) ## 100
     parser.add_argument('--epochs', type=int, default=300)
     parser.add_argument('--steps', type=int, default=1000)
