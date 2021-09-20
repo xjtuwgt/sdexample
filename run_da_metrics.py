2a5ea8aeae46a0a37d6d3be6c4d00c1ac4ee52da
guangtao.wang <guangtao.wang@jd.com>
2021-09-20 10:00:37 -0700

    argument parser

--- run_da_metrics.py
+++ run_da_metrics.py
@@ -26,7 +26,6 @@ orig_dev_dataloader = orig_da_dev_data_loader(args=args)
 
 drop_train_dataloader = drop_da_train_data_loader(args=args)
 drop_dev_data_loader = drop_da_dev_data_loader(args=args)
-# test_dataloader = test_data_loader(args=args)
 #+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 # torch.cuda.manual_seed_all(args.seed)
 seed_everything(seed=args.seed)
@@ -46,67 +45,8 @@ if args.drop_model_name is not None and args.orig_model_name is not None:
                                                       args=args)
 else:
     diversity_metrics = 0.0
-
 for key, value in vars(args).items():
     print(key, value)
 
 print('Affinity metrics: {}'.format(affinity_metrics))
 print('Diversity metrics: {}'.format(diversity_metrics))
-
-# model = model_builder(args=args)
-# model = model.to(args.device)
-# optimizer = torch.optim.AdamW(model.parameters(), lr=args.lr, weight_decay=1e-2)
-# #+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-# step = 0
-# start_epoch = 0
-# best_dev_acc = -1
-# test_acc = -1
-# best_step = None
-# window_step = 0
-# # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-# train_iterator = trange(start_epoch, start_epoch + int(args.epochs), desc="Epoch")
-# for epoch_idx, epoch in enumerate(train_iterator):
-#     epoch_iterator = tqdm(train_dataloader, desc="Iteration")
-#     train_correct = 0
-#     train_total = 0
-#     for batch_idx, batch in enumerate(epoch_iterator):
-#         model.train()
-#         batch = {k: batch[k].to(args.device) for k in batch}
-#         input = batch['input'].clamp(min=0)
-#         attn_mask = (input >= 0)
-#         loss, logits = model(input, attention_mask=attn_mask, labels=batch['labels'])
-#
-#         optimizer.zero_grad()
-#         loss.backward()
-#         torch.nn.utils.clip_grad_norm_(model.parameters(), 1.0)
-#         optimizer.step()
-#         pred = logits.max(1)[1]
-#         train_total = train_total + pred.shape[0]
-#         train_correct += (pred == batch['labels']).sum()
-#         if (step + 1) % args.eval_batch_interval_num == 0:
-#             dev_acc = model_evaluation(model=model, data_loader=dev_dataloader, args=args)
-#             if dev_acc > best_dev_acc:
-#                 best_dev_acc = dev_acc
-#                 # test_acc = model_evaluation(model=model, data_loader=test_dataloader, args=args)
-#                 if args.save_model:
-#                     model_name = join(args.exp_name, 'model_{}_{}_{}_{}_dev_{:.4f}.pkl'.format(args.beta_drop, args.sent_dropout,
-#                                                                                                epoch_idx+1, batch_idx+1, dev_acc))
-#                     save_match_model(model=model, model_name=model_name)
-#                 best_step = (epoch + 1, step + 1)
-#                 window_step = 0
-#             else:
-#                 window_step = window_step + 1
-#             print("Step {}: dev accuracy={:.6f}, current best dev accuracy={:.6f} and test accuracy = {:.6f}".format((epoch + 1, step + 1), dev_acc, best_dev_acc, test_acc), flush=True)
-#             if window_step >= args.window_size:
-#                 break
-#         if window_step >= args.window_size:
-#             break
-#         step = step + 1
-#     print('Train accuracy = {:.6f} at {}'.format(train_correct *1.0 /train_total, epoch))
-#     if window_step >= args.window_size:
-#         break
-# print("Best dev result at {} dev accuracy={:.6f} test accuracy = {:.6f}".format(best_step, best_dev_acc, test_acc))
-# print('*'*25)
-# for key, value in vars(args).items():
-#     print('{}\t{}'.format(key, value))
-# print('*' * 25)
