b6ff707f999a0f581b7cbeb1d3109649a526a55b
guangtao.wang <guangtao.wang@jd.com>
2021-09-20 14:43:35 -0700

    argument parser

--- run_da_metrics.py
+++ run_da_metrics.py
@@ -8,6 +8,9 @@ from envs import OUTPUT_FOLDER
 from tqdm import tqdm, trange
 from os.path import join
 import os
+from data_utils.da_metrics_utils import MODEL_NAMES
+
+model_dict = {_[0]: _[1] for _ in MODEL_NAMES}
 
 parser = data_aug_default_parser()
 args = parser.parse_args()
@@ -16,6 +19,7 @@ args = complete_default_parser(args=args)
 # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 if args.exp_name is None:
     args.exp_name = args.train_file_name + '.models'
+    model_names = model_dict[args.exp_name]
     args.exp_name = join(OUTPUT_FOLDER, args.exp_name)
     os.makedirs(args.exp_name, exist_ok=True)
 for key, value in vars(args).items():
@@ -29,24 +33,24 @@ drop_dev_data_loader = drop_da_dev_data_loader(args=args)
 #+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 # torch.cuda.manual_seed_all(args.seed)
 seed_everything(seed=args.seed)
-if args.orig_model_name is not None:
-    orig_model = load_pretrained_model(args=args, pretrained_model_name=args.orig_model_name)
-    affinity_metrics = affinity_metrics_computation(model=orig_model, dev_data_loader=orig_dev_dataloader,
-                                                    drop_dev_data_loader=drop_dev_data_loader, args=args)
-else:
-    affinity_metrics = 0.0
-
-if args.drop_model_name is not None and args.orig_model_name is not None:
-    drop_model = load_pretrained_model(args=args, pretrained_model_name=args.orig_model_name)
-    orig_model = load_pretrained_model(args=args, pretrained_model_name=args.orig_model_name)
-    diversity_metrics = diversity_metrics_computation(model=orig_model, drop_model=drop_model,
-                                                      train_data_loader=orig_train_dataloader,
-                                                      drop_train_data_loader=drop_train_dataloader,
-                                                      args=args)
-else:
-    diversity_metrics = 0.0
-for key, value in vars(args).items():
-    print(key, value)
-
-print('Affinity metrics: {}'.format(affinity_metrics))
-print('Diversity metrics: {}'.format(diversity_metrics))
+# if args.orig_model_name is not None:
+#     orig_model = load_pretrained_model(args=args, pretrained_model_name=args.orig_model_name)
+#     affinity_metrics = affinity_metrics_computation(model=orig_model, dev_data_loader=orig_dev_dataloader,
+#                                                     drop_dev_data_loader=drop_dev_data_loader, args=args)
+# else:
+#     affinity_metrics = 0.0
+#
+# if args.drop_model_name is not None and args.orig_model_name is not None:
+#     drop_model = load_pretrained_model(args=args, pretrained_model_name=args.orig_model_name)
+#     orig_model = load_pretrained_model(args=args, pretrained_model_name=args.orig_model_name)
+#     diversity_metrics = diversity_metrics_computation(model=orig_model, drop_model=drop_model,
+#                                                       train_data_loader=orig_train_dataloader,
+#                                                       drop_train_data_loader=drop_train_dataloader,
+#                                                       args=args)
+# else:
+#     diversity_metrics = 0.0
+# for key, value in vars(args).items():
+#     print(key, value)
+#
+# print('Affinity metrics: {}'.format(affinity_metrics))
+# print('Diversity metrics: {}'.format(diversity_metrics))
