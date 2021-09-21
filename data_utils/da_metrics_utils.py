938fde0aa0eec47bab451fedfb0ab49bbb4a7ea6
guangtao.wang <guangtao.wang@jd.com>
2021-09-20 17:05:56 -0700

    argument parser

--- data_utils/da_metrics_utils.py
+++ data_utils/da_metrics_utils.py
@@ -41,7 +41,11 @@ def affinity_metrics_computation(model, dev_data_loader, drop_dev_data_loader, a
     Affinity: Acc(model, drop_dev) / Acc(model, dev)
     """
     model = model.to(args.device)
-    drop_acc = model_evaluation(model=model, data_loader=drop_dev_data_loader, args=args)
+    drop_acc_list = []
+    for i in range(10):
+        drop_acc = model_evaluation(model=model, data_loader=drop_dev_data_loader, args=args)
+        drop_acc_list.append(drop_acc)
+    drop_acc = sum(drop_acc_list)/len(drop_acc_list)
     acc = model_evaluation(model=model, data_loader=dev_data_loader, args=args)
     print('Drop accuracy: {}, orig accuracy = {}'.format(drop_acc, acc))
     affinity = drop_acc/acc
