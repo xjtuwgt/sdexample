918acbf42a19d5ff1bc4622346b6235dc6ec072f
guangtao.wang <guangtao.wang@jd.com>
2021-09-20 19:43:38 -0700

    argument parser

--- run_da_metrics.py
+++ run_da_metrics.py
@@ -49,6 +49,42 @@ def da_affinity_metrics_collection(args):
 
     return affinity_metrics
 
+def da_diversity_metrics_collection(args):
+    # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    # if args.exp_name is None:
+    args.exp_name = args.train_file_name + '.models'
+    model_name_dict = model_dict[args.exp_name]
+    args.exp_name = join(OUTPUT_FOLDER, args.exp_name)
+    os.makedirs(args.exp_name, exist_ok=True)
+    args.orig_model_name = join(args.exp_name, model_name_dict['orig'])
+    if args.beta_drop:
+        args.drop_model_name = join(args.exp_name, model_name_dict['beta_drop'])
+    else:
+        args.drop_model_name = join(args.exp_name, model_name_dict['drop'])
+    for key, value in vars(args).items():
+        print('{}\t{}'.format(key, value))
+    print('*' * 50)
+    for idx, (key, value) in enumerate(model_dict.items()):
+        for k, v in value.items():
+            print(idx + 1, key, k, v)
+    print('*' * 50)
+    # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    orig_train_dataloader = orig_da_train_data_loader(args=args)
+    drop_train_dataloader = drop_da_train_data_loader(args=args)
+    # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    seed_everything(seed=args.seed)
+    if args.drop_model_name is not None and args.orig_model_name is not None:
+        drop_model = load_pretrained_model(args=args, pretrained_model_name=args.drop_model_name)
+        orig_model = load_pretrained_model(args=args, pretrained_model_name=args.orig_model_name)
+        diversity_metrics = diversity_metrics_computation(model=orig_model, drop_model=drop_model,
+                                                          train_data_loader=orig_train_dataloader,
+                                                          drop_train_data_loader=drop_train_dataloader,
+                                                          args=args)
+    else:
+        diversity_metrics = 0.0
+    return diversity_metrics
+
 
 if __name__ == '__main__':
     train_file_names = ['train_fastsingle_cat_100_42_300_0.5.pkl.gz',
