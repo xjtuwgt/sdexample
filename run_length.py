25bf5e534d39d1591e53ba9605d887053793982b
guangtao.wang <guangtao.wang@jd.com>
2021-09-22 09:04:45 -0700

    argument parser

--- run_length.py
+++ run_length.py
@@ -1,6 +1,6 @@
 from data_utils.argument_parser import data_aug_default_parser, complete_default_parser
 from toyexample_experiments import seed_everything, dev_data_loader
-from data_utils.da_metrics_utils import affinity_metrics_computation, diversity_metrics_computation
+from data_utils.model_utils import model_evaluation
 import torch
 from data_utils.model_utils import load_pretrained_model
 from envs import OUTPUT_FOLDER
@@ -33,8 +33,12 @@ def accuracy_collection(args):
     # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
     seed_everything(seed=args.seed)
     orig_model = load_pretrained_model(args=args, pretrained_model_name=orig_model_name)
+    orig_acc = model_evaluation(model=orig_model, data_loader=dev_dataloader, args=args)
     drop_model = load_pretrained_model(args=args, pretrained_model_name=drop_model_name)
+    drop_acc = model_evaluation(model=drop_model, data_loader=dev_dataloader, args=args)
     beta_drop_model = load_pretrained_model(args=args, pretrained_model_name=beta_drop_model_name)
+    beta_acc = model_evaluation(model=beta_drop_model, data_loader=dev_dataloader, args=args)
+    return orig_acc, drop_acc, beta_acc
 
 
 
@@ -48,7 +52,33 @@ if __name__ == '__main__':
                         'train_fastsingle_cat_10000_42_300_0.5.pkl.gz',
                         'train_fastsingle_cat_20000_42_300_0.5.pkl.gz']
 
-    dev_file_names = []
+    dev_file_names = ['eval_fastsingle_cat_10000_2345_350_0.5.pkl.gz',
+                      'eval_fastsingle_cat_10000_2345_325_0.5.pkl.gz',
+                      'eval_fastsingle_cat_10000_2345_300_0.5.pkl.gz',
+                      'eval_fastsingle_cat_10000_2345_275_0.5.pkl.gz',
+                      'eval_fastsingle_cat_10000_2345_250_0.5.pkl.gz',
+                      'eval_fastsingle_cat_10000_2345_225_0.5.pkl.gz',
+                      'eval_fastsingle_cat_10000_2345_175_0.5.pkl.gz',
+                      'eval_fastsingle_cat_10000_2345_150_0.5.pkl.gz',
+                      'eval_fastsingle_cat_10000_2345_125_0.5.pkl.gz',
+                      'eval_fastsingle_cat_10000_2345_100_0.5.pkl.gz',
+                      'eval_fastsingle_cat_10000_2345_50_0.5.pkl.gz']
     parser = data_aug_default_parser()
     args = parser.parse_args()
     args = complete_default_parser(args=args)
+
+    accuracy_list = []
+    for train_file_name in train_file_names:
+        args.train_file_name = train_file_name
+        accuracy_sub_list = []
+        for eval_file_name in dev_file_names:
+            args.eval_file_name = eval_file_name
+            orig_acc, drop_acc, beta_acc = accuracy_collection(args=args)
+            accuracy_sub_list.append((eval_file_name, orig_acc, drop_acc, beta_acc))
+        accuracy_list.append((train_file_name, accuracy_sub_list))
+
+    for _ in accuracy_list:
+        print(_[0])
+        for x in _[1]:
+            print(x)
+        print('*' * 75)
